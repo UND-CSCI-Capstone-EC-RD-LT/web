e1e074370feb6db9a9310461d3c9d2bf5da6b6b7
raymonddavis <raymond.davis.4th@gmail.com>
2017-02-15 21:04:12 -0600

    AngularJs Boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name undimswebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the undimswebApp
+ */
+angular.module('undimswebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
