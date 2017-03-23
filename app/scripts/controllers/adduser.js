b433e857b78d428e8e47a956f1c3988a61c4979f
Edwin Carlson <eddie.carlson5@gmail.com>
2017-03-23 00:00:41 -0500

    create add user controller, view and route create add user form and inputs

---
+++ app/scripts/controllers/adduser.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name undimswebApp.controller:AdduserCtrl
+ * @description
+ * # AdduserCtrl
+ * Controller of the undimswebApp
+ */
+angular.module('undimswebApp')
+  .controller('AdduserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    console.log('add user');
+  });
