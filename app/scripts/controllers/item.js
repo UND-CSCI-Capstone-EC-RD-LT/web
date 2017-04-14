09553de3d4fd43bbf62453c23170b230a9855597
Edwin Carlson <eddie.carlson5@gmail.com>
2017-04-14 13:38:19 -0500

    get item by id from stateparams add table with dummy data.

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -7,10 +7,40 @@
  * # ItemCtrl
  * Controller of the undimswebApp
  */
-angular.module('undimswebApp').controller('ItemCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+angular.module('undimswebApp').controller('ItemCtrl', function ($scope, $stateParams, $Item) {
+
+	console.log($stateParams.id);
+
+	$Item.get($stateParams.id).then(function (res) {
+		console.log(res.data);
+        $scope.item = res.data[0];
+    }, function (error) {
+        Toast.error({
+            content: { details: { error } }
+        });
+    });
+
+    $scope.scans = [
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"},
+    	{date: "4/14/17 1:30 PM", building: "Streibel Hall", room: 109, scanner: "Eddie Carlson", price: "200.00"}
     ];
 });
