3b79ebc89ed1d8210cc6392ac78b6c1e3b29ea3a
Raymond Davis <raymond.davis.4th@gmail.com>
2017-04-25 18:35:54 -0500

    Implemented Radar Graph

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -10,8 +10,6 @@
 angular.module('undimswebApp').controller('StatsCtrl', function($scope, $Logs) {
   const fixTimestamp = (data) => data.map((item) => {
     item.timestamp = moment(item.createdAt);
-    delete item.updatedAt;
-    delete item.createdAt;
     return item;
   });
   const hoursBack = (data, hrs = 24) => data.filter((item) => moment().unix() >= item.timestamp.unix() && moment().unix() - (60 * 60 * hrs) <= item.timestamp.unix() );
@@ -67,14 +65,34 @@ angular.module('undimswebApp').controller('StatsCtrl', function($scope, $Logs) {
     return output;
   };
 
+  const radarActions = (data) => {
+    let output = [...new Array($scope.activity.series.length)].map((item) => new Array($scope.actions.labels.length).fill(0));
+
+    for (let i in data) {
+      for( let j in data[i]) {
+          let action = j === 'find' || j === 'findOne' || j === 'findone' ? 'search' : j;
+          output[$scope.activity.series.indexOf(i)][$scope.actions.labels.indexOf(action)] += j.length;
+      }
+    }
+
+    return output;
+  };
+
+
   $Logs.getAll().then((res) => {
     $scope.logs = {};
     $scope.day = setupData(hoursBack(fixTimestamp(res.data), 24));
     $scope.week = setupData(hoursBack(fixTimestamp(res.data), (24 *7)));
-    //$scope.activity = line24($scope.day); //Last Day View
+
     $scope.activity = line7($scope.week); //Last Week
-    console.log($scope.activity);
+    $scope.actions = {};
+    $scope.actions.labels = ['create', 'delete', 'update', 'search'];
+    $scope.actions.series = $scope.activity.series;
+    $scope.actions.data = radarActions($scope.week);
   });
 
-  $scope.changeGraph = (type) => $scope.activity = type === 7 ? line7($scope.week) : line24($scope.day);
+  $scope.changeData = (type) => {
+      $scope.activity = type === 7 ? line7($scope.week) : line24($scope.day);
+      $scope.actions.data  = type === 7 ? radarActions($scope.week) : radarActions($scope.day);
+  }
 });
---
+++ app/scripts/services/Auth.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name Auth
+ * @description
+ * Service to create a Auth
+ */
+angular.module('undimswebApp').service('Auth', function ($cookies) {
+  return {
+    set : (token, user) => {
+      let now = new Date();
+      let expires = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 15);
+      $cookies.put('token', token, { expires });
+      $cookies.putObject('user', user, { expires });
+    },
+    get : (type) =>  $cookies.get(type),
+    refreah: () => {
+      let now = new Date();
+      let expires = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 15);
+      $cookies.put('token', $cookies.get('token'), { expires });
+      $cookies.putObject('user', $cookies.get('user'), { expires });
+    }
+  };
+});
