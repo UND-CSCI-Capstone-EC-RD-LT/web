b65e8259fa23ae215c9dcb20a6d8b807932928b0
Raymond Davis <raymond.davis.4th@gmail.com>
2017-04-27 01:17:52 -0500

    New Department Files

---
+++ app/scripts/controllers/department.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name undimswebApp.controller:DepartmentsCtrl
+ * @description
+ * # DepartmentsCtrl
+ * Controller of the undimswebApp
+ */
+angular.module('undimswebApp').controller('DepartmentsCtrl', function ($scope, $mdDialog, $Department, Toast) {
+  $scope.selected = [];
+
+	($scope.load = () => {
+		$Department.getAllWithBuildings().then((res) => {
+			$scope.departments = res.data.map(item => {
+        item.buildingCount = item.buildings.length;
+        return item;
+      });
+		}, (error) => Toast.error({ details: { content: error.data.message } }));
+	})();
+
+  $scope.displayTime = timestamp => moment(timestamp).format('h:m a M/D/Y');
+
+  $scope.filter = (type) => {
+    let ASC = type.slice(0, 1) === '-' ? false : true;
+    let attr = ASC ? type : type.slice(1, type.length);
+    $scope.departments.sort((a, b) => {
+      let A;
+      let B;
+      if (typeof a[attr] == 'number') {
+        A = a[attr];
+        B = b[attr];
+      } else {
+        A = a[attr].toUpperCase();
+        B = b[attr].toUpperCase();
+      }
+      if (!ASC) [A, B] = [B, A];
+      if (A < B) return -1;
+      if (A > B) return 1;
+      return 0;
+    });
+  };
+
+  $scope.add = (ev) => {
+    $mdDialog.show({
+      controller: 'AddDepartmentCtrl',
+      templateUrl: './views/addDepartment.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    }).then(function(answer) {
+      $scope.load();
+      Toast.success();
+    }, function() {
+        $scope.status = 'You cancelled the dialog.';
+    });
+  };
+
+  $scope.delete = () => Promise.all($scope.selected.map(item => $Department.delete(item.id))).then(res => {
+      $scope.load();
+      Toast.success();
+      $scope.selected = [];
+    }, error => Toast.error({ details: { content: error.data.message } }));
+}).controller('AddDepartmentCtrl', function ($scope, $mdDialog, $Department, Toast, Auth) {
+
+  $scope.new = {
+    name: '',
+  };
+
+  $scope.hide = () => $mdDialog.hide();
+
+  $scope.cancel = () => $mdDialog.cancel();
+
+  $scope.add = () => {
+      $scope.new.creator = Auth.getUser().id;
+      $Department.create($scope.new).then(res => $mdDialog.hide(true), error => Toast.error({ details: { content: error.data.message } }));
+  };
+
+});
